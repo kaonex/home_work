0c54a1e81f3ffee769ddd84b55922e6c6555d265
kaonex <562485479@qq.com>
2021-01-05 17:26:25 +0800

    第六次提交

--- src/day06/Test05.java
+++ src/day06/Test05.java
@@ -1,4 +1,8 @@
 package day06;
+
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * 要求用户输入一个文件或目录名，并删除当前目录下的该文件或目录。
  * 可自行手动先在当前项目目录中创建一个要删除的文件或目录，若是目录，还可以
@@ -8,6 +12,9 @@ package day06;
  */
 public class Test05 {
     public static void main(String[] args) {
+        String name=new Scanner(System.in).next();
+        File file = new File(name);
+        file.delete();
 
     }
 
--- src/day06/Test07.java
+++ src/day06/Test07.java
@@ -1,4 +1,8 @@
 package day06;
+
+import java.io.*;
+import java.util.Scanner;
+
 /**
  * 要求用户输入一个文件名，并复制当前目录中该文件，并取名为"原文件名_copy.后缀名"
  * 定义两个方法分别使用单字节形式复制，以及字节数组形式复制
@@ -6,21 +10,45 @@ package day06;
  *
  */
 public class Test07 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		String name=new Scanner(System.in).next();
+		copy1(name);
+
+		copy2(name);
+
 	}
 	/**
 	 * 使用单字节方式复制
 	 * @param name 要复制的文件名
 	 */
-	public void copy1(String name){
-		
+	public static void copy1(String name)throws IOException{
+		InputStream fis = new FileInputStream(name);
+		String[] split = name.split("\\.");
+		OutputStream fos = new FileOutputStream(split[0] + "_copy" + split[1]);
+
+		int read;
+		while((read=fis.read())!=-1){
+			fos.write(read);
+		}
+		fis.close();
+		fos.close();
+
+
 	}
 	/**
 	 * 使用字节数组形式复制
 	 * @param name 要复制的文件名
 	 */
-	public void copy2(String name){
+	public static void copy2(String name)throws IOException{
+		InputStream fis = new FileInputStream(name);
+		String[] split = name.split("\\.");
+		OutputStream fos = new FileOutputStream(split[0] + "_copy." + split[1]);
+
+		byte[] reads=new byte[fis.available()];
+		fos.write(reads);
+		fis.close();
+		fos.close();
+
 		
 	}
 }
